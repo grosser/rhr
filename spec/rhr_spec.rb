978c37039f29027f4844914cfef43768846fbb5b
sdepold <sascha@depold.com>
2011-11-04 15:17:05 +0100

    layout spec

--- spec/rhr_spec.rb
+++ spec/rhr_spec.rb
@@ -24,89 +24,104 @@ describe RHR do
       Dir.chdir 'spec/site'
     end
 
-    def app
-      RHR::Server.new
+    describe 'without layout' do
+      def app
+        app = RHR::Server.new
+        app.stub!(:find_layout).and_return nil
+        app
+      end
+
+      it "can get /" do
+        get '/'
+        last_response.body.should == 'TEST'
+      end
+
+      it "evaluates erb files" do
+        get '/index.erb'
+        last_response.body.should == 'TEST'
+        last_response.content_type.should == 'text/html'
+      end
+
+      it "can get static files" do
+        get '/plain.html'
+        last_response.body.should == "<%= 'TEST' %>\n"
+      end
+
+      it "does not server monkey paths" do
+        get '/xxx/../plain.html'
+        last_response.status.should == 404
+      end
+
+      it "can get nested index.erb" do
+        get '/xxx'
+        last_response.body.should == "NESTED"
+      end
+
+      it "can get nested index.erb with trailing slash" do
+        get '/xxx/'
+        last_response.body.should == "NESTED"
+      end
+
+      it "can get nested index without template language" do
+        get '/yyy'
+        last_response.body.should == "Foo\n"
+      end
+
+      it "can get static files with correct content-type" do
+        get "/people.jpg"
+        last_response.content_type.should == 'image/jpeg'
+      end
+
+      it "ignores common ruby files" do
+        get "/Gemfile"
+        last_response.status.should == 404
+      end
+
+      it "ignores _files" do
+        get "/_hidden.html"
+        last_response.status.should == 404
+      end
+
+      it "ignores .files" do
+        get "/.hidden.html"
+        last_response.status.should == 404
+      end
+
+      it "ignores hidden folders" do
+        get "/_hidden/xxx.html"
+        last_response.status.should == 404
+      end
+
+      it "ignores nested hidden" do
+        get "/xxx/.hidden.html"
+        last_response.status.should == 404
+      end
+
+      it "passes env" do
+        get "/xxx/env.erb"
+        last_response.body.should == "/xxx/env.erb"
+      end
+
+      it "passes get params" do
+        get "/xxx/params.erb?x=1&a%20b=b%20c"
+        last_response.body.should == "[[\"a b\", \"b c\"], [\"x\", \"1\"]]"
+      end
+
+      it "passes merged post and get params" do
+        post "/xxx/params.erb?a=1&b=2", {'b' => 3}
+        last_response.body.should == "[[\"a\", \"1\"], [\"b\", \"3\"]]"
+      end
     end
 
-    it "can get /" do
-      get '/'
-      last_response.body.should == 'TEST'
-    end
+    describe 'with layout' do
+      def app
+        RHR::Server.new
+      end
 
-    it "evaluates erb files" do
-      get '/index.erb'
-      last_response.body.should == 'TEST'
-      last_response.content_type.should == 'text/html'
-    end
-
-    it "can get static files" do
-      get '/plain.html'
-      last_response.body.should == "<%= 'TEST' %>\n"
-    end
-
-    it "does not server monkey paths" do
-      get '/xxx/../plain.html'
-      last_response.status.should == 404
-    end
-
-    it "can get nested index.erb" do
-      get '/xxx'
-      last_response.body.should == "NESTED"
-    end
-
-    it "can get nested index.erb with trailing slash" do
-      get '/xxx/'
-      last_response.body.should == "NESTED"
-    end
-
-    it "can get nested index without template language" do
-      get '/yyy'
-      last_response.body.should == "Foo\n"
-    end
-
-    it "can get static files with correct content-type" do
-      get "/people.jpg"
-      last_response.content_type.should == 'image/jpeg'
-    end
-
-    it "ignores common ruby files" do
-      get "/Gemfile"
-      last_response.status.should == 404
-    end
-
-    it "ignores _files" do
-      get "/_hidden.html"
-      last_response.status.should == 404
-    end
-
-    it "ignores .files" do
-      get "/.hidden.html"
-      last_response.status.should == 404
-    end
-
-    it "ignores hidden folders" do
-      get "/_hidden/xxx.html"
-      last_response.status.should == 404
-    end
-
-    it "ignores nested hidden" do
-      get "/xxx/.hidden.html"
-      last_response.status.should == 404
-    end
-
-    it "passes env" do
-      get "/xxx/env.erb"
-      last_response.body.should == "/xxx/env.erb"
-    end
-
-    it "passes get params" do
-      get "/xxx/params.erb?x=1&a%20b=b%20c"
-      last_response.body.should == "[[\"a b\", \"b c\"], [\"x\", \"1\"]]"
-    end
-
-    it "passes merged post and get params" do
-      post "/xxx/params.erb?a=1&b=2", {'b' => 3}
-      last_response.body.should == "[[\"a\", \"1\"], [\"b\", \"3\"]]"
+      it 'renders the layout if available' do
+        get '/'
+        last_response.body.should == "<div>TEST</div>\n"
+      end
     end
   end
 end
